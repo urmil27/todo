ab3de0eec3c3f1e5149a5b6cf0761421c9b8f405
urmil27 <urmilsakariya329@gmail.com>
2024-07-12 20:32:16 +0530

    3rd card

--- src/compo/Foreground.jsx
+++ src/compo/Foreground.jsx
@@ -24,6 +24,13 @@ function Foreground() {
       close: false,
       tag: { isOpen: true, tagtitle : "Upload" ,tagColor:"#622323" },
     }
+    ,
+    {
+      desc: "Jay shree Krishna Jay khoodiyar maa Jay hanuman dada ",
+      filesize: ".9mb",
+      close: false,
+      tag: { isOpen: true, tagtitle : "Upload" ,tagColor:"#622323" },
+    }
   ];
   return (
     <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-2 flex-wrap p-5">
